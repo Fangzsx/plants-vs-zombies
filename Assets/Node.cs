3847b893e0554511a491293da77175bc40476c1b
Fangszx <jygrzn@gmail.com>
2021-03-14 15:51:46 +0800

    added nodes for placing plants

---
+++ Assets/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private Color startColor;
+    private Renderer rend;
+
+    public Color hoverColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
