84059adaf65a836bb869acb83e20abdb34051492
Fangszx <jygrzn@gmail.com>
2021-03-14 11:49:11 +0800

    Initial commit

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public static Transform[] points;
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+
+    private void Update()
+    {
+        RandomizeSpawnPoint();
+    }
+
+    void RandomizeSpawnPoint()
+    {
+        int randomIndex = Random.Range(0, points.Length);
+        Debug.Log(points[randomIndex]);
+
+    }
+}
