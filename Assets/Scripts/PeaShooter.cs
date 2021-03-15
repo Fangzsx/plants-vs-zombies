10b9ae37829ceba8d3b0d65f7c21765f10b79b61
Fangszx <jygrzn@gmail.com>
2021-03-15 23:11:59 +0800

    added range display using gizmoz

---
+++ Assets/Scripts/PeaShooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeaShooter : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float shootingRange = 5f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void UpdateTarget()
+    {
+        
+    }
+}
