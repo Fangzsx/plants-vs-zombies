be05b60324ddb6b687cce4f153342554bd05d33a
Fangszx <jygrzn@gmail.com>
2021-03-16 21:24:35 +0800

    target seek
    added tag for zombies,
    to implement target seeking in pea shooter.cs

--- Assets/Scripts/PeaShooter.cs
+++ Assets/Scripts/PeaShooter.cs
@@ -19,12 +19,37 @@ public class PeaShooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating(nameof(UpdateTarget), 0f, 0.5f);
     }
 
     // Update is called once per frame
     void UpdateTarget()
     {
-        
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag("zombie");
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestZombie = null;
+
+        foreach (GameObject zombie in zombies)
+        {
+            
+            if (transform.position.z == zombie.transform.position.z)
+            {
+
+                float distanceToZombie = Vector3.Distance(transform.position, zombie.transform.position);
+                if(distanceToZombie < shortestDistance)
+                {
+                    nearestZombie = zombie;
+                }
+
+            }
+        }
+
+        if(nearestZombie != null && shortestDistance <= shootingRange)
+        {
+            target = nearestZombie.transform;
+            Debug.Log(gameObject + "nearest enemy " + target);
+        }
+
     }
 }
